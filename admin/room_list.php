cfa73869fde5c8699c0e1f3c74eb1791f868e627
ryoichi-k <kazyuapple99@gmail.com>
2021-07-08 17:10:19 +0900

    fix:room_list.php

--- admin/room_list.php
+++ admin/room_list.php
@@ -14,6 +14,104 @@ try {
     header('Content-Type: text/plain; charset=UTF-8', true, 500);
     exit($e->getMessage());
 }
+//id sort desc
+if (!empty($_POST['id-desc'])) {
+    $id_array = array();
+    foreach( $rooms as $value) {
+        $id_array[] = $value['id'];
+    }
+        array_multisort(
+                $id_array,
+                SORT_DESC,
+                SORT_NUMERIC,
+                $rooms,
+                );
+}
+//id sort asc
+if (!empty($_POST['id-asc'])) {
+    $id_array = array();
+    foreach( $rooms as $value) {
+        $id_array[] = $value['id'];
+    }
+        array_multisort(
+                $id_array,
+                SORT_ASC,
+                SORT_NUMERIC,
+                $rooms,
+                );
+}
+//name sort desc
+if (!empty($_POST['name-desc'])) {
+    $id_array = array();
+    $name_array = array();
+    foreach($rooms as $value) {
+        $id_array[] = $value['id'];
+        $name_array[] = $value['name'];
+    }
+    array_multisort(
+        $name_array,
+        SORT_DESC,
+        SORT_STRING,
+        $id_array,
+        SORT_DESC,
+        SORT_NUMERIC,
+        $rooms,
+        );
+}
+//name sort asc
+if (!empty($_POST['name-asc'])) {
+    $id_array = array();
+    $name_array = array();
+    foreach($rooms as $value) {
+        $id_array[] = $value['id'];
+        $name_array[] = $value['name'];
+    }
+    array_multisort(
+        $name_array,
+        SORT_ASC,
+        SORT_STRING,
+        $id_array,
+        SORT_DESC,
+        SORT_NUMERIC,
+        $rooms,
+        );
+}
+//updated_at sort desc
+if (!empty($_POST['updated_at-desc'])) {
+    $id_array = array();
+    $updated_at_array = array();
+    foreach($rooms as $value) {
+        $id_array[] = $value['id'];
+        $updated_at_array[] = $value['updated_at'];
+    }
+    array_multisort(
+        $updated_at_array,
+        SORT_DESC,
+        SORT_NUMERIC,
+        $id_array,
+        SORT_DESC,
+        SORT_NUMERIC,
+        $rooms,
+        );
+}
+//updated_at sort asc
+if (!empty($_POST['updated_at-asc'])) {
+    $id_array = array();
+    $updated_at_array = array();
+    foreach($rooms as $value) {
+        $id_array[] = $value['id'];
+        $updated_at_array[] = $value['updated_at'];
+    }
+    array_multisort(
+        $updated_at_array,
+        SORT_ASC,
+        SORT_NUMERIC,
+        $id_array,
+        SORT_DESC,
+        SORT_NUMERIC,
+        $rooms,
+        );
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -46,17 +144,29 @@ try {
         <?php getPage();?>
             <table class="room_list-table" border="1">
                 <tr>
-                    <th>
+                <th>
                         <form action="" method="post">
-                            <input type="submit" name="asc" value="▲"><br>
+                            <input type="submit" name="id-asc" value="▲"><br>
                                 ID<br>
-                            <input type="submit" name="desc" value="▼">
+                            <input type="submit" name="id-desc" value="▼">
                         </form>
                     </th>
-                    <th>部屋名</th>
+                    <th>
+                        <form action="" method="post">
+                            <input type="submit" name="name-asc" value="▲"><br>
+                                部屋名<br>
+                            <input type="submit" name="name-desc" value="▼">
+                        </form>
+                    </th>
                     <th>画像</th>
                     <th>登録日時</th>
-                    <th>更新日時</th>
+                    <th>
+                        <form action="" method="post">
+                            <input type="submit" name="updated_at-asc" value="▲"><br>
+                            更新日時<br>
+                            <input type="submit" name="updated_at-desc" value="▼">
+                        </form>
+                    </th>
                     <th><button onclick="location.href='./room_edit.php'">新規登録</button></th>
                 </tr>
                 <?php foreach ($rooms as $room):?>
